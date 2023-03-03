cc67809dc77b6454f9c35fca3a00476df2138b60
Christian Fowler <cfowlerdev@gmail.com>
2023-03-03 21:37:49 +0800

    Debug mouse and keyboard changes

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,6 +1,73 @@
 #include <iostream>
 #include "engine.h"
 
+static void windowPosCallback(GLFWwindow* window, int x, int y)
+{
+    std::cout << ">>> windowPosCallback(" << x << ", " << y << ")" << std::endl;
+}
+
+static void windowSizeCallback(GLFWwindow* window, int width, int height)
+{
+    std::cout << ">>> windowSizeCallback(" << width << ", " << height << ")" << std::endl;
+}
+
+static void windowMaximizeCallback(GLFWwindow* window, int maximized)
+{
+    if(maximized)
+    {
+        std::cout << ">>> windowMaximizeCallback : MAXIMIZED" << std::endl;    
+    }
+    else
+    {
+        std::cout << ">>> windowMaximizeCallback : UN-MAXIMIZED" << std::endl;
+    }
+}
+
+static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+    std::cout << ">>> mouseButtonCallback(" << button << ", " << action << ", " << mods << ")" << std::endl;
+}
+
+static void cursorPosCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    std::cout << ">>> cursorPosCallback(" << xpos << ", " << ypos << ")"  << std::endl;
+}
+
+static void cursorEnterCallback(GLFWwindow* window, int entered)
+{
+    if(entered)
+    {
+        std::cout << ">>> Mouse entered <<<" << std::endl;
+    } else {
+        std::cout << ">>> Mouse left <<<" << std::endl;
+    }
+}
+
+static void scrollCallback(GLFWwindow* window, double x, double y)
+{
+    std::cout << ">>> scrollCallback(" << x << ", " << y << ")" << std::endl;
+}
+
+static void charCallback(GLFWwindow* window, unsigned int codepoint)
+{
+    std::cout << ">>> charCallback(" << codepoint << ")" << std::endl;
+}
+
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    std::cout << ">>> keyCallback(" << key << ", " << scancode << ", " << action << " , " << mods << ")" << std::endl;
+}
+
+static void dropCallback(GLFWwindow* window, int count, const char** paths)
+{
+    std::cout << ">>> dropCallback(" << count << ")" << std::endl;
+}
+
+static void errorCallback(int error, const char* description)
+{
+    std::cout << "GLFW ERROR(" << error << ": " << description << std::endl;
+}
+
 Engine::Engine(const std::string &p_title, const int &p_width, const int &p_height)
     : width(p_width), height(p_height)
 {
@@ -25,20 +92,25 @@ Engine::Engine(const std::string &p_title, const int &p_width, const int &p_heig
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     glClearColor(0.0, 0.0, 0.0, 0.0);
 
-    // TODO: Set callbacks
-    // glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-    // glfwSetCursorPosCallback(window, mouseCallback);
-    // glfwSetScrollCallback(window, scrollCallback);
-    
-    // capture mouse
-    glfwSetInputMode(windowId, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    // Callbacks
+	glfwSetWindowPosCallback(windowId, windowPosCallback);
+	glfwSetWindowSizeCallback(windowId, windowSizeCallback);
+	// glfwSetWindowMaximizeCallback(windowId, windowMaximizeCallback);
+	glfwSetMouseButtonCallback(windowId, mouseButtonCallback);
+	glfwSetCursorPosCallback(windowId, cursorPosCallback);
+	glfwSetCursorEnterCallback(windowId, cursorEnterCallback);
+	glfwSetScrollCallback(windowId, scrollCallback);
+	glfwSetCharCallback(windowId, charCallback);
+	glfwSetKeyCallback(windowId, keyCallback);
+	glfwSetDropCallback(windowId, dropCallback);
 
     // configure global opengl state
     glEnable(GL_DEPTH_TEST);
-
 }
 
 Engine::~Engine()
 {
     glfwTerminate();
 }
+
+
